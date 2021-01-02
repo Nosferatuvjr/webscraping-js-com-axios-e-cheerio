5961ff14770bf96718329009a51ae88bc5ac7dd8
Vivaldo Chagas <67665267+Nosferatuvjr@users.noreply.github.com>
2021-01-02 12:02:49 -0300

    first

---
+++ webscraping.js
@@ -0,0 +1,11 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const fetchData = async(url) => {
+    const result = axios.get('url')
+    return result.data
+}
+
+const main = () => {
+    
+}
